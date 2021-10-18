947677d4ef2adb0c89dcfe0d9fbc826a24594a26
Suraj vishwakarma <90332766+vishwakarmasuraj@users.noreply.github.com>
2021-10-18 14:07:00 +0530

    at 2.06 pm
    all done API filter ad date wise

--- constant/student.js
+++ constant/student.js
@@ -3,7 +3,8 @@ const constants = {
     ROUND: 10,
     LIST_MSG: 'Found record successfully',
     STD_AVAIL_MSG: 'Student is existing within this date',
-    STD_NOTAVAIL_MSG: 'Student does not existing within this date'
+    STD_NOTAVAIL_MSG: 'Student does not existing within this date',
+    ENSURE_MSG_ERR: 'Please ensure you pick two dates'
 }
 
 module.exports = Object.freeze(constants)
--- controller/student.js
+++ controller/student.js
@@ -1,40 +1,49 @@
 const Student = require('../models/student')
 const constants = require('./../constant/student')
-const {successHandler, errorHandler} = require('./../helper/responseHandler')
+const { successHandler, errorHandler } = require('./../helper/responseHandler')
 const bcrypt = require('bcrypt')
 
-
 const addStudent = async (req, res) => {
-    try {
-        req.body.password = await bcrypt.hash(req.body.password, constants.ROUND)
-        const student = await new Student(req.body)
-        await student.save()
-        successHandler(res, constants.CREATE_MSG)
-    } catch (error) {
-        console.error(error)
-        errorHandler(res)
-    }
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, constants.ROUND)
+    const student = await new Student(req.body)
+    await student.save()
+    successHandler(res, constants.CREATE_MSG)
+  } catch (error) {
+    console.error(error)
+    errorHandler(res)
+  }
 }
 
 const studentListing = async (req, res) => {
-    try {
-        const result = await Student.find({})
-        successHandler(res, constants.LIST_MSG, result)
-    } catch (error) {
-        console.error(error)
-        errorHandler(res)
-    }
+  try {
+    const result = await Student.find({})
+    successHandler(res, constants.LIST_MSG, result)
+  } catch (error) {
+    console.error(error)
+    errorHandler(res)
+  }
 }
 
 const studentFilterRecord = async (req, res) => {
-    try {
-        const result = await Student.find({})
-        successHandler(res, constants.STD_AVAIL_MSG, result)
-    } catch (error) {
-        console.error(error)
-        errorHandler(res)
+  try {
+    let { startDate, endDate } = req.query
+    if (startDate || endDate) {
+      return res
+        .status(400)
+        .json({ message: 'please ensure you pick two dates' })
     }
+    const result = await Student.find({
+      dateOfAdmission: {
+        $gte: new Date(startDate).setHours(00, 00, 00),
+        $lt: new Date(endDate).setHours(23, 59, 59),
+      },
+    }).sort({ dateOfAdmission: 'asc' })
+    res.status(200).json({ result })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ msg: 'something went wrong' })
+  }
 }
 
-
-module.exports = {addStudent, studentListing, studentFilterRecord}
+module.exports = { addStudent, studentListing, studentFilterRecord }
